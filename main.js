b2fbd41f3814a32989b431cfe10a0ed2299748f8
Gowthamy19 <gowthamyjeyathashan@gmail.com>
2021-09-13 21:25:12 +0530

    My second push

--- main.js
+++ main.js
@@ -39,3 +39,151 @@ $(document).ready(function() {
      });
 
     });
+
+    SC.initialize({
+      client_id: '340f063c670272fac27cfa67bffcafc4'
+    });
+    $(document).ready(function() {
+
+      SC.stream('/tracks/18625859',function(sound){
+
+        $('#start2').click(function(e) {
+                 e.preventDefault();
+                 sound.start();
+               });
+               $('#stop2').click(function(e) {
+                e.preventDefault();
+                sound.stop();
+                });
+
+
+       });
+
+      });
+
+      SC.initialize({
+        client_id: '340f063c670272fac27cfa67bffcafc4'
+      });
+      $(document).ready(function() {
+
+        SC.stream('/tracks/54489278',function(sound){
+
+          $('#start3').click(function(e) {
+                   e.preventDefault();
+                   sound.start();
+                 });
+                 $('#stop3').click(function(e) {
+                  e.preventDefault();
+                  sound.stop();
+                  });
+
+
+         });
+
+        });
+
+        SC.initialize({
+          client_id: '340f063c670272fac27cfa67bffcafc4'
+        });
+        $(document).ready(function() {
+
+          SC.stream('/tracks/345946410',function(sound){
+
+            $('#start4').click(function(e) {
+                     e.preventDefault();
+                     sound.start();
+                   });
+                   $('#stop4').click(function(e) {
+                    e.preventDefault();
+                    sound.stop();
+                    });
+
+
+           });
+
+          });
+
+          SC.initialize({
+            client_id: '340f063c670272fac27cfa67bffcafc4'
+          });
+          $(document).ready(function() {
+
+            SC.stream('/tracks/345819962',function(sound){
+
+              $('#start5').click(function(e) {
+                       e.preventDefault();
+                       sound.start();
+                     });
+                     $('#stop5').click(function(e) {
+                      e.preventDefault();
+                      sound.stop();
+                      });
+
+
+             });
+
+            });
+
+
+            SC.initialize({
+              client_id: '340f063c670272fac27cfa67bffcafc4'
+            });
+            $(document).ready(function() {
+
+              SC.stream('/tracks/348359524',function(sound){
+
+                $('#start6').click(function(e) {
+                         e.preventDefault();
+                         sound.start();
+                       });
+                       $('#stop6').click(function(e) {
+                        e.preventDefault();
+                        sound.stop();
+                        });
+
+
+               });
+
+              });
+
+              SC.initialize({
+                client_id: '340f063c670272fac27cfa67bffcafc4'
+              });
+              $(document).ready(function() {
+
+                SC.stream('/tracks/134808621',function(sound){
+
+                  $('#start7').click(function(e) {
+                           e.preventDefault();
+                           sound.start();
+                         });
+                         $('#stop7').click(function(e) {
+                          e.preventDefault();
+                          sound.stop();
+                          });
+
+
+                 });
+
+                });
+
+                SC.initialize({
+                  client_id: '340f063c670272fac27cfa67bffcafc4'
+                });
+                $(document).ready(function() {
+
+                  SC.stream('/tracks/517722948',function(sound){
+
+                    $('#start8').click(function(e) {
+                             e.preventDefault();
+                             sound.start();
+                           });
+                           $('#stop8').click(function(e) {
+                            e.preventDefault();
+                            sound.stop();
+                            });
+
+
+                   });
+
+                  });
